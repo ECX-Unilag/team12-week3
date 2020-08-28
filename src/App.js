f1216c1e89613b1087b029b103953e49de3433d4
Femi Lawal <lawalshalom@gmail.com>
2020-08-28 13:45:20 +0100

    usestate rm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Route, Switch } from 'react-router-dom';
 import AOS from "aos";
 import Home from "./Pages/Home";
