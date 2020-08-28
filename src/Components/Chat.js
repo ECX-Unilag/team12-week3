4f8a53df172369232a6fb064a4567f780cd71cf0
Femi Lawal <lawalshalom@gmail.com>
2020-08-28 15:00:51 +0100

    chat added

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -4,21 +4,7 @@ const Chat = () => {
 
     return (
         <div id="live-chat">
-            <div className="chat-open">
-                <div className="chat-title">
-                    <h5>Hi there</h5>
-                    <p>It's Lateef here, do you have any question?</p>
-                </div>
-                <div className="text-area">
-                    <div className="text-messages"></div>
-                    <form id="chat-message">
-                        <input type="name" name="message" required placeholder="Type a message..."/>
-                        <button type="submit"><i className="fa fa-arrow-right"></i></button>
-                    </form>
-                </div>
-            </div>
-            <div className="chat-btn">. . .</div>
-            <div className="btn-arrow"></div>
+            <div class="elfsight-app-0104f104-879b-462e-bfb6-b9e668550217"></div>
         </div>
     )
 }
